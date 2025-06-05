b723a7f13a8432edec3f4cfb6830b49c4e5d3780
Jack <forebacj@mail.gvsu.edu>
2025-06-05 11:07:34 -0400

    Updated Linear Classifier to loop over multiple seeds and plot average accuracy

--- LinearClassifier.py
+++ LinearClassifier.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
+from sample_plotting import plot_samples
 
 # Set variables
-torch.manual_seed(42)
 num_classes = 3
-num_training_steps = 20
+num_training_steps = 100
 input_dim = 2
 samples_per_class = 100
 train_ratio = 0.7
@@ -30,8 +30,8 @@ for class_id in range(num_classes):
     x = torch.distributions.MultivariateNormal(means[class_id], covs[class_id]).sample((samples_per_class,))
     X.append(x)
 
-# Copy for plotting
-cp = X
+# Plot the data
+plot_samples(X, num_classes)
 
 # Create corresponding class labels
 classes = [torch.tensor([1.0, 0.0, 0.0]), torch.tensor([0.0, 1.0, 0.0]), torch.tensor([0.0, 0.0, 1.0])]
@@ -52,24 +52,6 @@ split_idx = int(train_ratio * total_samples)
 X_train, Y_train = X[:split_idx], Y[:split_idx]
 X_test, Y_test = X[split_idx:], Y[split_idx:]
 
-# Model definition
-class LinearClassifier(nn.Module):
-    def __init__(self, input_dim: int, num_classes: int):
-        super().__init__()
-        self.linear = nn.Linear(input_dim, num_classes)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.linear(x)
-
-model = LinearClassifier(input_dim, num_classes)
-
-# Loss function and optimizer
-criterion = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01)
-
-# Dict to keep track of classification accuracy per class every step
-accuracy_dict = {i: [] for i in range(num_classes)}
-
 # Calculate number of samples from each class in the test set
 test_samples_per_class = [0, 0, 0]
 for i in Y_test:
@@ -80,34 +62,63 @@ for i in Y_test:
     else:
         test_samples_per_class[2] += 1
 
-# Function to track accuracy of each class
-def track_accuracy(predictions):
+# Dict to store average classification accuracy at each step
+accuracy_dict = {i: [0] * 100 for i in range(num_classes)}
+
+# Function to track the totalcnumber of accurate classifications at each step
+def track_accuracy(predictions, current_step):
     tmp_arr = [0, 0, 0]
-    # Check how many correct classifications made for each class
+    # Check how many correct classifications for each class
     for i in range(len(predictions)):
         idx = torch.argmax(predictions[i])
         if (idx == torch.argmax(Y_test[i])):
             tmp_arr[idx] += 1
 
-    # Add percent of correctly clasified test data to accuracy_dict
+    # Add number of correctly clasified test data to correct step spot in accuracy_dict
     for i in range(num_classes):
-        accuracy_dict[i].append(tmp_arr[i] / test_samples_per_class[i])
+        accuracy_dict[i][j] += tmp_arr[i]
 
-# Training loop
-for _ in range(num_training_steps):
-    y_pred = model(X_train)
-    loss = criterion(y_pred, Y_train)
+# Model definition
+class LinearClassifier(nn.Module):
+    def __init__(self, input_dim: int, num_classes: int):
+        super().__init__()
+        self.linear = nn.Linear(input_dim, num_classes)
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    
-    # Test loop
-    with torch.no_grad():
-        y_pred = model(X_test)
-        track_accuracy(y_pred)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear(x)
 
-# print means and covariance to be piped into file with images to identify each test
+
+# Model Instantiation. Set seeds for 100 trials with random weights
+for i in range(100):
+    torch.manual_seed(i)
+    model = LinearClassifier(input_dim, num_classes)
+
+    # Loss function and optimizer
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+    # Training loop
+    for j in range(num_training_steps):
+        y_pred = model(X_train)
+        loss = criterion(y_pred, Y_train)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # Test loop
+        with torch.no_grad():
+            y_pred = model(X_test)
+            track_accuracy(y_pred, j)
+
+# Divides the number of correct classifications at each step location in the list
+# by the total number of possible correct classifications over the # of training steps
+for i in range(3):
+    div = test_samples_per_class[i]*num_training_steps
+    for j in range(len(accuracy_dict[i])):
+        accuracy_dict[i][j] /= div
+
+# Print means and covariance to be piped into file with images to identify the distributions used in each test
 print("Means:", means, "\n", "Covs:", covs)
 
 # Accuracy plotting
@@ -121,30 +132,5 @@ plt.title("Per-Class Test Accuracy")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("tests/test1/accuracy_graph.png")
-plt.close()
-
-# Sample plotting
-colors = ['green', 'blue', 'purple']
-labels = [f"Class {i}" for i in range(num_classes)]
-
-plt.figure(figsize=(10, 6))
-
-# Plot original class samples
-for class_id in range(num_classes):
-    samples = cp[class_id]
-    plt.scatter(samples[:, 0], samples[:, 1],
-                # alpha=0.5,
-                color=colors[class_id],
-                label=labels[class_id])
-
-# Plot formatting
-plt.title("Generated 2D Gaussian Samples with Test Points")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.axis("equal")
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.savefig("tests/test1/sample_plot.png")
+plt.savefig("tests/test3/accuracy_graph.png")
 plt.close()
